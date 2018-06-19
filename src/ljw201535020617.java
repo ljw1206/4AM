acd2d4ef8ce3486cc796d0984db0466f411c4488
梁家威 <2674774101@qq.com>
2018-06-19 23:51:44 +0800

    第一次提交代码

---
+++ src/ljw201535020617.java
@@ -0,0 +1,38 @@
+
+public class ljw201535020617 {
+    //学号：201535020617
+	//姓名：梁家威
+	private Integer id;       // 主键id
+	private String username; // 客户名称
+	private String jobs;      // 职业
+	private String phone;     // 电话
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getJobs() {
+		return jobs;
+	}
+	public void setJobs(String jobs) {
+		this.jobs = jobs;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", username=" + username + 
+				       ", jobs=" + jobs + ", phone=" + phone + "]";
+	}
+}
